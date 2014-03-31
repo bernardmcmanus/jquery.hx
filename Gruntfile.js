a705c1fb1d28d7980d1eff3b9f6c106593dd7f59
Bernard McManus <bmcmanus@millennialmedia.com>
2014-03-31 12:17:51 -0400

    updating gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,9 +19,9 @@ module.exports = function( grunt ) {
 
     grunt.initConfig({
 
-            pkg: grunt.file.readJSON('package.json'),
+            pkg: grunt.file.readJSON( 'package.json' ),
 
-            clean : ["hx-*"],
+            clean : [ 'hx-*' ],
 
             uglify : {
                 options : {
@@ -34,9 +34,9 @@ module.exports = function( grunt ) {
                 }
             },
 
-            "git-describe" : {
-                "options" : {
-                    prop : "git-version"
+            'git-describe' : {
+                'options' : {
+                    prop : 'git-version'
                 }
             },
 
@@ -46,11 +46,11 @@ module.exports = function( grunt ) {
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-git-describe');
+    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+    grunt.loadNpmTasks( 'grunt-contrib-clean' );
+    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks( 'grunt-git-describe' );
 
-    grunt.registerTask('default', ['jshint','git-describe', 'clean', 'uglify']);
+    grunt.registerTask( 'default' , [ 'jshint' , 'git-describe' , 'clean' , 'uglify' ]);
 
 };
